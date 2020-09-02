a0821af0be69cdc4107ed0cc3fd22583e2c4b4e8
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-09-02 22:58:03 +0300

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+void bubble_Sort(int arr[], int size) {
+
+  for (int j = 0; j < size - 1; ++j) {
+    for (int i = 0; i < size - j - 1; ++i) {
+        if (arr[i] > arr[i + 1]) {
+        int temp = arr[i];
+        arr[i] = arr[i + 1];
+        arr[i + 1] = temp;
+      }
+    }
+  }
+}
