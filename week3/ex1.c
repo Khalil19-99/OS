bd322d938f4bd4117a71f23b81c7a3ea295fbb74
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-09-02 23:06:41 +0300

    Update ex1.c

--- week3/ex1.c
+++ week3/ex1.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 int main(){
-int* pc;
-int c;
-c=22;
-
-printf("Address of c:%04x\n",&c);
-printf("Value of c:%04x\n\n",c);
-pc=&c;
-printf("Address of pointer pc:%04x\n",pc);
-printf("Content of pointer pc:%04x\n\n",*pc);
-c=11;
-printf("Address of pointer pc:%04x\n",pc);
-printf("Content of pointer pc:%04x\n\n",*pc);
-*pc=2;
-printf("Address of c:%04x\n",&c);
-printf("Value of c:%04x\n\n",c);
-return 0;
+ int* pc;
+ int c;
+ c=22;
+ printf("Address of c:%p\n",&c);
+ printf("Value of c:%d\n\n",c);
+ pc=&c;
+ printf("Address of pointer pc:%p\n",pc);
+ printf("Content of pointer pc:%d\n\n",*pc);
+ c=11;
+ printf("Address of pointer pc:%p\n",pc);
+ printf("Content of pointer pc:%d\n\n",*pc);
+ *pc=2;
+ printf("Address of c:%p\n",&c);
+ printf("Value of c:%d\n\n",c);
+ return 0;
 }
