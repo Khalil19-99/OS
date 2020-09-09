5f3142f85f33a5aff5393f70d729c2e91002d1de
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-09-09 22:58:15 +0300

    Update ex2.c

--- week 4/ex2.c
+++ week 4/ex2.c
@@ -4,10 +4,42 @@
 
 int main()
 {
-for(int i=0;i<3;i++)
+for(int i=0;i<5;i++)
 {
     fork();
 }
 sleep(seconds:5);
     return 0;
 }
+
+/*
+The `pstree` will be the following:
+├─zsh───ex2─┬─ex2─┬─ex2───ex2
+│           │     └─ex2
+│           ├─ex2───ex2
+│           └─ex2
+
+first, each instance of `ex2` forks itself then,
+Each new process becomes a child of the old process, again and again till the end of the loop
+In the end of execution there should be 2^3 = 8 processes.
+
+
+when the loop is till 5, the tree will contain 2^5=32 processes:
+
+─zsh─┬─ex2─┬─ex2─┬─ex2─┬─ex2─┬─ex2───ex2
+     │     │     │     │     └─ex2
+     │     │     │     ├─ex2───ex2
+     │     │     │     └─ex2
+     │     │     ├─ex2─┬─ex2───ex2
+     │     │     │     └─ex2
+     │     │     ├─ex2───ex2
+     │     │     └─ex2
+     │     ├─ex2─┬─ex2─┬─ex2───ex2
+     │     │     │     └─ex2
+     │     │     ├─ex2───ex2
+     │     │     └─ex2
+     │     ├─ex2─┬─ex2───ex2
+     │     │     └─ex2
+     │     ├─ex2───ex2
+     │     └─ex2
+
