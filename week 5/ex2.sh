a030cad23f3548ebf13bbb7b6f016fdd949228eb
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-09-16 16:35:49 +0300

    Update ex2.sh

--- week 5/ex2.sh
+++ week 5/ex2.sh
@@ -1,18 +1,21 @@
-if test | -f numbers 
+if test ! -f numbers 
 then 
- echo 0>numbers
+ echo 0 > numbers
 fi 
 
-#locks and do not let interrupts
 if ln numbers numbers.lock
  then 
-#repeat 100 times for fun 
-for i in 'seq 1 1000';
-do 
-n='tail -1 numbers '
-expr  $n+1>>numbers 
-done 
-
-#Unlock
-rm numbers.lock
+ for i in 'seq 1 1000';
+ do 
+  n='tail -1 numbers '
+  expr  $n+1>>numbers 
+ done 
+ 
+ #Unlock
+ rm numbers.lock
 fi 
+
+# the error may occur when you execute: `bash ex2.sh; bash ex2.sh &`.
+# race condition may occur, when both scripts want to access the file and write to it -> this will make garbage/not correct results in file.
+# to prevent this, we can use `ln` to check, whether file is occupied by another process or not.
+# critical region is a shared variables/data structures/memory segments, concurrent access of which can lead to unexpected beahaviour.
