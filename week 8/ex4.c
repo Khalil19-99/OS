8b36a57990f03f05a457e48df6c4a1fb2bc474b6
Khalil19-99 <67523643+Khalil19-99@users.noreply.github.com>
2020-10-06 14:49:09 +0300

    Add files via upload

---
+++ week 8/ex4.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <errno.h>
+
+extern int errno;
+
+int main()
+{
+    struct rusage * usage =malloc(sizeof(struct rusage));
+    int mb=1024*1024;
+    for (int i=0;i<10;i++){
+       memset(calloc(10*mb,1),0,10*mb);
+       if(getrusage(RUSAGE_SELF,usage)){
+           printf("Error %d\n",errno);
+       };
+       printf("ru_maxrss=%ld/n",(*usage).ru_maxrss);
+       sleep(1);
+    }
+    return 0;
+}
